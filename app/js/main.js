cec5b72e882aa6e440bc28b0ad4142bd4169616a
jo-svg <144480253+jo-svg@users.noreply.github.com>
2024-10-30 12:25:26 -0400

    sdadas

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,15 @@
 import '../css/style.css';
+import {} from "./products";
+
 //.. is to go to anotehr file wrd
+//cd app
+//npm run dev
+
+
+
+// proj idea:
+// find popular songs 
+
+
+// have a section for the different themes
+// toggle button for night, day, and sunset mayb
---
+++ app/js/products.js
@@ -0,0 +1,45 @@
+const popularSongs = [
+    {
+      title: "Song Title 1",
+      artist: "Artist Name 1",
+      genre: "Genre 1",
+      releaseDate: "YYYY-MM-DD",
+      imageUrl: "",
+      altText: "Description of the image for Song Title 1"
+    },
+    {
+      title: "Song Title 2",
+      artist: "Artist Name 2",
+      genre: "Genre 2",
+      releaseDate: "YYYY-MM-DD",
+      imageUrl: "",
+      altText: "Description of the image for Song Title 2"
+    },
+    {
+      title: "Song Title 3",
+      artist: "Artist Name 3",
+      genre: "Genre 3",
+      releaseDate: "YYYY-MM-DD",
+      imageUrl: "",
+      altText: "Description of the image for Song Title 3"
+    },
+    {
+      title: "Song Title 4",
+      artist: "Artist Name 4",
+      genre: "Genre 4",
+      releaseDate: "YYYY-MM-DD",
+      imageUrl: "",
+      altText: "Description of the image for Song Title 4"
+    },
+    {
+      title: "Song Title 5",
+      artist: "Artist Name 5",
+      genre: "Genre 5",
+      releaseDate: "YYYY-MM-DD",
+      imageUrl: "",
+      altText: "Description of the image for Song Title 5"
+    }
+];
+
+export {popularSongs};
+  
