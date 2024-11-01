ecb1d08c67e7a681b3577553afefab98de21980d
jo-svg <Josephinel43@nycstudents.net>
2024-11-01 00:58:49 -0400

    i8 crash out

--- app/js/main.js
+++ app/js/main.js
@@ -4,11 +4,10 @@ import { popularSongs } from "./products";
 //.. is to go to anotehr file wrd
 //cd app
 //npm run dev
-
+const container = document.querySelector(".container");
 //adds the cards from the products.js file
 document.addEventListener("DOMContentLoaded", ()=>{
     popularSongs.forEach(song=> {
-        const container = document.querySelector(".container");
         const title = song.title;
         const artist = song.artist; 
         const genre = song.genre;
@@ -31,7 +30,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
 })
 
 //click toggle button 
-const setting = document.querySelector(".setting-btn")
+
+const setting = document.querySelector('.setting-btn');
 setting.addEventListener("click", ()=>{
+    const ul = document.querySelector('.ul-setting')
+    setting.classList.toggle('-toggle')
+    let icon = setting.querySelector('.bi-moon');
+    if (!icon) {
+       
+        icon = document.createElement('i');
+        icon.className = 'bi bi-moon';
+        // Insert the icon inside the setting button
+        ul.insertAdjacentElement('beforeend', icon);
+    } else {
+        // If the icon already exists, remove it on toggle off
+        icon.remove();
+    }
+
     
 })
+
