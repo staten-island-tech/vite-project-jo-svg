9494138cadde3ff0c74aafbd2d7ff9c4d4320aaa
jo-svg <Josephinel43@nycstudents.net>
2024-11-01 23:05:49 -0400

    ds

---
+++ app/js/dropdown.js
@@ -0,0 +1,9 @@
+const dropdowns = document.querySelectorAll('.filtering');
+dropdowns.forEach(dropdown=>{
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu li');
+    const selected = dropdown.querySelector('.selected');
+    
+})
